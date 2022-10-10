7c93e9d56b2a3e4a4f035e03eb86516dd72ec45d
polina_storozh <58592174+voird@users.noreply.github.com>
2022-10-10 22:24:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,7 @@ var res = document.getElementById('result');
 
 ok.addEventListener('click', function(){
   var x = a.value; 
-  var y = b.value; // Это еще строки, не числа
-  
-  var sum = +x * +y; // Плюсик перед строкой - превращает её в число.
-  
-  /* Перед тем, как вывести результат - идет проверка условия
-  && - значит 'И'. А NaN или пустая "" строка в логическом контексте будут false 
-  Чтобы оно выплнилось, и sum не должен быть NaN, и строки не должны быть пустыми. 
-  использована функция isNaN, потому что иначе 0 + 0 будет false */
+  var y = b.value; 
+  var sum = +x * +y; 
   res.textContent = ( !isNaN(sum) && x && y ) ? sum : 'Введите числа!';
 });
