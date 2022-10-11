4ef5e08797bc7c7be7ea98fc8a65aed46eb25bc5
polina_storozh <58592174+voird@users.noreply.github.com>
2022-10-11 17:59:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,27 @@ var b = document.getElementById('b');
 var ok = document.getElementById('ok');
 var res = document.getElementById('result');
 
+function isNumeric( x ) {
+  return (/^[\d]+$/g).test( x );
+}
+
 ok.addEventListener('click', function(){
   var x = a.value; 
   var y = b.value; 
-  var sum = +x * +y;
-  res.textContent = ( !isNaN(sum) && x && y && (x<0) && (y<0)) ? sum : 'Введите числа!';
+  if( !isNumeric(x) ) {
+    alert( "Введено некорректное значение" );
+    return;
+  }
+  if( !isNumeric(y) ) {
+    alert( "Введено некорректное значение" );
+    return;
+  }
+  var sum = parseInt(x)*parseInt(y);
+  res.textContent = sum;
 });
+window.addEventListener("DOMContentLoaded", function (event) {
+  console.log("DOM fully loaded and parsed");
+  let button = document.getElementById("ok");
+  button.addEventListener("click", ok);
+  });
+
