04248633d16f2a5bb654cb8dcf56241bdad03665
polina_storozh <58592174+voird@users.noreply.github.com>
2022-10-11 18:27:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-var a = document.getElementById('a');
-var b = document.getElementById('b');
-var ok = document.getElementById('ok');
-var res = document.getElementById('result');
+let a = document.getElementById("a");
+let b = document.getElementById("b");
+let ok = document.getElementById("ok");
+let res = document.getElementById("result");
 
-function isNumeric( x ) {
-  return (/^[\d]+$/g).test( x );
+function isNumeric(x) {
+    return (/^[\d]+$/g).test(x);
 }
 
-ok.addEventListener('click', function(){
-  var x = a.value; 
-  var y = b.value; 
-  if( !isNumeric(x) ) {
-    alert( "Введено некорректное значение" );
-    return;
-  }
-  if( !isNumeric(y) ) {
-    alert( "Введено некорректное значение" );
-    return;
-  }
-  var sum = parseInt(x)*parseInt(y);
-  res.textContent = sum;
+ok.addEventListener("click", function() {
+    let x = a.value;
+    let y = b.value;
+    if (!isNumeric(x)) {
+        alert("Введено некорректное значение");
+        return;
+    }
+    if (!isNumeric(y)) {
+        alert("Введено некорректное значение");
+        return;
+    }
+    let sum = parseInt(x) * parseInt(y);
+    res.textContent = sum;
 });
-window.addEventListener("DOMContentLoaded", function (event) {
-  console.log("DOM fully loaded and parsed");
-  let button = document.getElementById("ok");
-  button.addEventListener("click", ok);
-  });
-
+window.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+    let button = document.getElementById("ok");
+    button.addEventListener("click", ok);
+});
